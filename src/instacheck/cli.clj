99352f47de46f50b9f4f6b2c29c47ac1b5a601a8
Joel Martin <github@martintribe.org>
2019-03-03 22:46:39 -0600

    Factor our cli commands into functions.

--- src/instacheck/cli.clj
+++ src/instacheck/cli.clj
@@ -63,69 +63,143 @@
                (str "Fail (exit code " (:exit res) ")")))
     (zero? (:exit res))))
 
+(defn save-weights [ctx file]
+  (when file
+    (pr-err "Saving weights to" file)
+    (spit file (with-out-str (pprint (into (sorted-map)
+                                           @(:weights-res ctx)))))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Command line usage of ebnf
 
 (def general-cli-options
-  [[nil "--debug" "Add debug comments to generated code"]
-   [nil "--verbose" "Verbose output during execution"]
+  [[nil "--debug"
+    "Add debug comments to generated code"]
+   [nil "--verbose"
+    "Verbose output during execution"]
    [nil "--weights WEIGHTS"
     "An EDN data file containing frequency weights"
     :default nil
     :parse-fn #(edn/read-string (slurp %))]
-   [nil "--weights-output WEIGHTS-OUTPUT" "Write all resulting frequency weights out the file."]
-   [nil "--start START" "Starting grammar rule"]
+   [nil "--weights-output WEIGHTS-OUTPUT"
+    "Write all resulting frequency weights out the file."]
+   [nil "--start START"
+    "Starting grammar rule"]
    ["-h" "--help"]])
 
 (def cmd-options
-  {"clj" [[nil "--function FUNCTION" "Generate one function that returns a map of generators"]]
-   "samples" [[nil "--samples SAMPLES" "Number of samples to generate"
+  {"clj"     [[nil "--function FUNCTION"
+               "Generate one function that returns a map of generators"]]
+   "samples" [[nil "--samples SAMPLES"
+               "Number of samples to generate"
                :default 10
                :parse-fn #(Integer. %)]]
-   "check" [["-s" "--seed SEED" "Random seed to use (otherwise automatic)"
+   "check"   [["-s" "--seed SEED"
+               "Random seed to use (otherwise automatic)"
                :parse-fn #(Integer. %)]
-            [nil "--iterations ITERATIONS" "Check/test iterations"
-             :default 10
-             :parse-fn #(Integer. %)]
-            [nil "--remove-samples" "Remove sample files after test"]]})
+              [nil "--iterations ITERATIONS"
+               "Check/test iterations"
+               :default 10
+               :parse-fn #(Integer. %)]
+              [nil "--remove-samples"
+               "Remove sample files after test"]]})
 
-;;(defn opt-errors [opts]
-;;  (when (:errors opts)
-;;    (doall (map pr-err (:errors opts)))
-;;    (System/exit 2))
-;;  opts)
+;; Gather up the general and command summary information
+(def cli-summary
+  (str "General Options:\n"
+       (:summary (parse-opts [] general-cli-options))
+       "\n"
+       (string/join \newline
+                    (for [[c co] cmd-options]
+                      (str "\n" c " Options:\n"
+                           (:summary (parse-opts [] co)))))))
 
-(defn usage [summary & [errors]]
+(defn usage [& [errors]]
   (when (not (empty? errors))
     (pr-err (string/join \newline errors) "\n"))
   (pr-err "Usage:")
   (pr-err "  instacheck clj     [OPTIONS] <EBNF-FILE> <NAMESPACE>")
   (pr-err "  instacheck samples [OPTIONS] <EBNF-FILE> <SAMPLE_TEMPLATE>")
-  (pr-err "  instacheck check   [OPTIONS] <EBNF-FILE> <SAMPLE_TEMPLATE> -- <CMD>")
   (pr-err "  instacheck parse   [OPTIONS] <EBNF-FILE> <FILE>...")
+  (pr-err "  instacheck check   [OPTIONS] <EBNF-FILE> <SAMPLE_TEMPLATE> -- <CMD>")
   (pr-err)
-  (pr-err summary)
+  (pr-err cli-summary)
   (System/exit 2))
 
-(defn save-weights [ctx file]
-  (when file
-    (spit file (with-out-str (pprint (into (sorted-map)
-                                           @(:weights-res ctx)))))))
+(defn do-clj
+  [ctx parser clj-ns function]
+  (when (not clj-ns)
+    (usage ["clj mode requires namespace"]))
+  (let [grammar (instacheck/parser->grammar parser)
+        gen-src (if function
+                  instacheck/grammar->generator-func-source
+                  instacheck/grammar->generator-defs-source)]
+
+    (println
+      (str (instacheck/clj-prefix clj-ns)
+           (gen-src ctx grammar)))))
+
+(defn do-samples
+  [ctx parser template number]
+  (when (not template)
+    (usage ["samples mode requires TEMPLATE path"]))
+  (let [grammar (instacheck/parser->grammar parser)
+        samples (gen/sample (instacheck/ebnf-gen ctx grammar) number)]
+    (output-samples ctx template samples)))
+
+(defn do-parse
+  [ctx parser files]
+  (when (empty? files)
+    (usage ["parse mode requires FILE list"]))
+  (let [weights
+        (try
+          (instacheck/parse-weights-from-files parser files)
+          (catch Exception e
+            (let [{:keys [text failure location]} (ex-data e)]
+              (println (str "Parse error in '" location "':"))
+              (println failure)
+              (System/exit 1))))]
+    ;; Update the ctx result weights
+    (reset! (:weights-res ctx) weights)))
+
+(defn do-check
+  [ctx parser template cmd opts]
+  (when (not template)
+    (usage ["check mode requires TEMPLATE path"]))
+  (when (empty? cmd)
+    (usage ["check mode requires CMD args"]))
+  (let [grammar (instacheck/parser->grammar parser)
+        cur-state (atom nil)
+        cur-idx (atom 0)
+        check-fn (fn [sample]
+                   (run-test ctx
+                             cmd
+                             (interpolate-path template (swap! cur-idx inc))
+                             sample))
+        report-fn (fn [r]
+                    (when (:verbose ctx)
+                      (prn :report (update-in
+                                     (dissoc r :property)
+                                     [:current-smallest] dissoc :function)))
+                    (when (not (= @cur-state (:type r)))
+                      (reset! cur-state (:type r))
+                      (pr-err (str "NEW STATE: " (name (:type r))))))
+        qc-res (instacheck/run-check
+                 opts
+                 (instacheck/ebnf-gen ctx grammar)
+                 check-fn report-fn)]
+    (println "Final Result:")
+    (pprint qc-res)
+    (when (not (:result qc-res))
+      (let [fpath (interpolate-path template "final")]
+        (spit fpath (get-in qc-res [:shrunk :smallest 0]))
+        (println "Smallest Failure:" fpath)))
+    (:result qc-res)))
 
 (defn -main
   [& args]
   (let [[[cmd & raw-args] [_ & check-cmd]] (split-with #(not= % "--") args)
-        ;; Gather up the general and command summary information
-        summary (str "General Options:\n"
-                     (:summary (parse-opts [] general-cli-options))
-                     "\n"
-                     (string/join \newline
-                                  (for [[c co] cmd-options]
-                                    (str "\n" c " Options:\n"
-                                         (:summary (parse-opts [] co))))))
-        usage (partial usage summary)
-
         ;; Now do the command specific parsing
         _ (when-not (#{"clj" "samples" "check" "parse"} cmd)
             (usage [(str "Unknown command " cmd)]))
@@ -142,83 +216,21 @@
                                       :grammar-updates])
                    {:weights-res (atom {})})
         ebnf-parser (instaparse/parser (slurp ebnf))
-        ebnf-grammar (instacheck/parser->grammar ebnf-parser)
 
         ;;_ (prn :ctx ctx)
 
-        ;; Some additional sanity checks not captured by the CLI parser
-        _ (when (and (= "clj" cmd) (not (= 1 (count cmd-args))))
-            (usage ["Too few arguments for clj command"]))
-
-        _ (when (and (= "samples" cmd) (not (= 1 (count cmd-args))))
-            (usage ["Too few arguments for samples command"]))
-
-        _ (when (and (= "check" cmd) (or (< (count cmd-args) 1)
-                                         (empty? check-cmd)))
-            (usage ["Too few arguments for check command"]))
-
         res (condp = cmd
               "clj"
-              (let [clj-ns (first cmd-args)
-                    gen-src (if (:function opts)
-                              instacheck/grammar->generator-func-source
-                              instacheck/grammar->generator-defs-source)]
-
-                (println
-                  (str (instacheck/clj-prefix clj-ns)
-                       (gen-src ctx ebnf-grammar))))
-
+              (do-clj ctx ebnf-parser (first cmd-args) (:function opts))
               "samples"
-              (let [sample-template (first cmd-args)
-                    samples (gen/sample (instacheck/ebnf-gen ctx ebnf-grammar)
-                                        (:samples opts))]
-                (output-samples ctx sample-template samples))
-
-              "check"
-              (let [sample-template (first cmd-args)
-                    cur-state (atom nil)
-                    cur-idx (atom 0)
-                    qc-res (instacheck/run-check
-                             (select-keys opts [:iterations :seed])
-                             (instacheck/ebnf-gen ctx ebnf-grammar)
-                             (fn [sample]
-                               (run-test ctx
-                                         check-cmd
-                                         (interpolate-path
-                                           sample-template (swap!  cur-idx inc))
-                                         sample))
-                             (fn [r]
-                               (when (:verbose ctx)
-                                 (prn :report (update-in
-                                                (dissoc r :property)
-                                                [:current-smallest] dissoc :function)))
-                               (when (not (= @cur-state (:type r)))
-                                 (reset! cur-state (:type r))
-                                 (pr-err (str "NEW STATE: " (name (:type r)))))))]
-                (println "Final Result:")
-                (pprint qc-res)
-                (when (not (:result qc-res))
-                  (let [fpath (interpolate-path sample-template "final")]
-                    (spit fpath (get-in qc-res [:shrunk :smallest 0]))
-                    (println "Smallest Failure:" fpath)))
-                (:result qc-res))
-
+              (do-samples ctx ebnf-parser (first cmd-args) (:samples opts))
               "parse"
-              (let [weights
-                    (try
-                      (instacheck/parse-weights-from-files
-                        ebnf-parser cmd-args)
-                      (catch Exception e
-                        (let [{:keys [text failure location]} (ex-data e)]
-                          (println (str "Parse error in '" location "':"))
-                          (println failure)
-                          (System/exit 1))))]
-                ;; Update the ctx result weights
-                (reset! (:weights-res ctx) weights)))]
+              (do-parse ctx ebnf-parser cmd-args)
+              "check"
+              (do-check ctx ebnf-parser (first cmd-args) check-cmd
+                        (select-keys opts [:iterations :seed])))]
 
-    (when-let [wfile (:weights-output opts)]
-      (pr-err "Saving weights to" wfile)
-      (save-weights ctx wfile))
+    (save-weights ctx (:weights-output opts))
 
     (if (= false res)
       (System/exit 1)
