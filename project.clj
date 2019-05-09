1ae062d8f1c6ad4e1a396aea1fb8d2969b233678
Joel Martin <github@martintribe.org>
2019-05-09 13:43:45 -0500

    Bump to 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kanaka/instacheck "0.4.2"
+(defproject kanaka/instacheck "0.5.0"
   :description "Property-based testing with inputs defined as EBNF grammars"
   :url "https://github.com/kanaka/instacheck"
   :license {:name "Mozilla Public License version 2"
